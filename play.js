5426de1a1db6a723480d7d514f94b266f1317b26
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-12-02 23:46:47 +0000

    Set an event listener for ctl + c key pair

--- play.js
+++ play.js
@@ -1,18 +1,22 @@
-const net = require("net");
-
-/**
- * Establishes connection with the game server
- */
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
+const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
