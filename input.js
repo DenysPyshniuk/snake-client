39c4585ff542e7491b01e9a7ee838f1848148a2b
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-12-03 00:59:57 +0000

    setup input so game is working just fine

--- input.js
+++ input.js
@@ -1,8 +1,13 @@
+// Stores the active TCP connection object.
+let connection;
+
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function () {
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,9 +17,21 @@ const setupInput = function () {
 };
 
 const handleUserInput = function (key) {
-  if (key === "\u0003") {
+  if(key === "\u0003") {
     process.exit();
+  } else if(key === "w") {
+    connection.write("Move: up");
+  } else if(key === "s") {
+    connection.write("Move: down");
+  } else if(key === "a") {
+    connection.write("Move: left");
+  } else if(key === "d") {
+    connection.write("Move: right");
+  } else if(key === "b") {
+    connection.write("Say: HELOOOOOOOOO!");
   }
 };
+
+
 
 module.exports = {setupInput}
